8cc2c6065659e79ac072419f3ef965a622cfad64
epodivilov <evgeniy.podivilov@gmail.com>
2015-10-29 10:49:00 +0500

    The function is changed. Added check operands. a XOR 0 = a a XOR a = 0

--- Task2.js
+++ Task2.js
@@ -1,12 +1,25 @@
 function maxXOR (startInterval, endInterval) {
     var maxXOR = 0;
-    for (var i = startInterval; i < endInterval; i++) {
+    for (var i = startInterval; i <= endInterval; i++) {
         for (var j = i; j <= endInterval; j++) {
-            var currentXOR = i ^ j;
-            if (maxXOR < currentXOR) {
-                maxXOR = currentXOR;
+            if (i != j) {
+                var currentXOR;
+                if (i === 0) {
+                    currentXOR = j;
+                } else if (j === 0) {
+                    currentXOR = i;
+                } else {
+                    currentXOR = i ^ j;
+                }
+
+                console.log("i= " + i + ", j= " + j + ", XOR= " + currentXOR);
+                if (maxXOR < currentXOR) {
+                    maxXOR = currentXOR;
+                }
             }
         }
     }
     return maxXOR;
 }
+
+console.log(maxXOR(-1,5));
